890cca6a3056dcc8d960b9dc3fbfa44ad1338d86
zhongkaizhu <zhongkaizhu@tencent.com>
2025-03-26 20:28:58 +0800

    Add demo

---
+++ cmd/proxydemo/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"github.com/mwitkow/grpc-proxy/proxy"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+func main() {
+	backendCC, err := grpc.Dial(
+		"localhost:23456", // perfdog addr
+		grpc.WithInsecure(),
+		grpc.WithBlock(),
+		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(10*1024*1024)))
+	if err != nil {
+		panic(err)
+		//return nil, fmt.Errorf("dialing backend: %v", err)
+	}
+	// First, we need to create a client connection to this backend.
+	// proxySrv := proxy.NewProxy(backendCC)
+
+	directorFn := func(ctx context.Context, fullMethodName string) (context.Context, *grpc.ClientConn, error) {
+		fmt.Printf("--> fullMethodName: %s\n", fullMethodName)
+		md, _ := metadata.FromIncomingContext(ctx)
+		fmt.Printf("--> fullMethodName: %s, md=%#v\n", fullMethodName, md)
+		outCtx := metadata.NewOutgoingContext(ctx, md.Copy())
+		return outCtx, backendCC, nil
+	}
+	proxySrv := grpc.NewServer(grpc.UnknownServiceHandler(proxy.TransparentHandler(directorFn)))
+	proxyBc, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", 9002))
+	if err != nil {
+		panic(err)
+	}
+
+	// run the proxy backend
+	if err := proxySrv.Serve(proxyBc); err != nil {
+		if err == grpc.ErrServerStopped {
+			return
+		}
+	}
+	proxySrv.GracefulStop()
+}
